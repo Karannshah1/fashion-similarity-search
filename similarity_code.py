0f404ce30223d22db6f6ecdde45fa31b77b5f911
Karannshah1 <80153010+Karannshah1@users.noreply.github.com>
2023-05-24 10:38:36 +0530

    Update similarity_code.py

--- similarity_code.py
+++ similarity_code.py
@@ -22,16 +22,7 @@ dimensions = 768
 
 pinecone.create_index(name = index_name, dimension = dimensions,metric = 'cosine')
 index = pinecone.Index(index_name=index_name)
-# index_name = "your_index_name"
-# index = pinecone.Index(index_name=index_name)
 
-# Load dataset from CSV file
-# dataset_path = "path/to/your/dataset.csv"
-# df = pd.read_csv(dataset_path)
-
-# df = pd.DataFrame(dataset)
-
-# # Example dataset
 
 df = pd.read_csv('result.csv')
 
@@ -46,7 +37,7 @@ temp = []
 vtry = []
 for data_point in dataset:
     # Tokenize and encode the input
-    inputs = tokenizer.encode_plus(data_point['id'], add_special_tokens=True, return_tensors='pt')
+    inputs = tokenizer.encode_plus(data_point['clean_text'], add_special_tokens=True, return_tensors='pt')
 
     
     # Pass the tokenized input through the BERT model
@@ -56,39 +47,7 @@ for data_point in dataset:
 
         pooled_embedding = torch.mean(embedding, dim=0)
         
-    temp.append(str(data_point['id']))
+    temp.append(str(data_point['clean_text']))
     vtry.append(pooled_embedding.numpy().astype(np.float32).tolist())
     
 index.upsert(vectors = zip(temp,vtry))
-
-
-# Perform similarity search
-query = "Men formal shoes"
-query_inputs = tokenizer.encode_plus(query, add_special_tokens=True, return_tensors='pt')
-with torch.no_grad():
-    query_outputs = model(**query_inputs)
-    query_embedding = query_outputs.last_hidden_state.squeeze(0)
-
-    query_pooled_list = torch.mean(query_embedding, dim=0).numpy()
-
-    
-
-# Retrieve similar results from Pinecone
-query_embedding_list = query_pooled_list.tolist()
-
-# Retrieve similar results from Pinecone
-
-response = index.query(queries=[query_embedding_list], top_k=5)
-print(response)
-similar_results = []
-indices = []
-for i in response.results[0].matches:
-  similar_results.append(i.id)
-  indices.append(i.score)
-
-
-
-# Retrieve the corresponding data points from the database using the identifiers
-results = [data_point for data_point in dataset if str(data_point['id']) in similar_results]
-
-print(results,indices)
